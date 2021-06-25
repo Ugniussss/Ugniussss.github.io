43da29c41dad3e782d5e821454d7d4c5be09c389
Ugniussss <gnsaura@gmail.com>
2021-06-25 03:41:21 +0300

    Commit 02
    Pakeičiamas kodas.

--- OrderManagementSystem/PHP/database.php
+++ OrderManagementSystem/PHP/database.php
@@ -3,10 +3,10 @@
 define('DB_NAME', 'order_management_system');
 define('DB_USER', 'root');
 define('DB_PASS', '');
-define('DB_HOST', 'localhost');
+define('DB_HOST', 'Ugniussss.github.io');
 
 
-$string = "mysql:host=localhost;dbname=order_management_system";
+$string = "mysql:host=Ugniussss.github.io;dbname=order_management_system";
 $connection = new PDO($string, DB_USER, DB_PASS);
 if (!$connection = new PDO($string, DB_USER, DB_PASS)) {
     die("Nepavyko prisijungti");
